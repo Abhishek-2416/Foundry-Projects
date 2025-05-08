1fea964e45da21d76308c499acf07aabba437056
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-05-09 03:23:30 +0530

    Complete ERC20 token and testing via CLI

--- 02-Foundry-ERC20/src/ManualToken.sol
+++ 02-Foundry-ERC20/src/ManualToken.sol
@@ -16,13 +16,15 @@ interface ERC20Interface {
     function balanceOf(address tokenOwner) external view returns(uint balance);
     function transfer(address to, uint tokens)external returns(bool success);
 
-    // function allowance(address tokenOwner,address spender) external view returns(uint remaning);
-    // function approve(address spender, uint tokens) external returns(bool success);
-    // function transferFrom(address from, address to, uint tokens) external returns(bool success);
+    function allowance(address tokenOwner,address spender) external view returns(uint remaning);
+    function approve(address spender, uint tokens) external returns(bool success);
+    function transferFrom(address from, address to, uint tokens) external returns(bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
-    // event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
+
+
 contract ManualToken is ERC20Interface{
     string public name = "AbhiToken";
     string public symbol = "ABHI";
@@ -30,7 +32,9 @@ contract ManualToken is ERC20Interface{
     uint public override totalSupply;
 
     address public founder;
+
     mapping (address => uint) public balances;
+    mapping (address => mapping(address => uint)) allowed;
 
     constructor(){
         totalSupply = 1000000;
@@ -51,6 +55,32 @@ contract ManualToken is ERC20Interface{
         balances[msg.sender] = balances[msg.sender] - tokens;   
 
         emit Transfer(msg.sender,to,tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner,address spender) view public override returns(uint remaning){
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens)public override returns(bool success){
+        require(balances[msg.sender] >= tokens);
+        require(tokens > 0);
+
+        allowed[msg.sender][spender] = tokens;
+
+        emit Approval(msg.sender,spender,tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint tokens)public override returns(bool success){
+        require(allowed[from][msg.sender] >= tokens);
+        require(balanceOf(from)>= tokens);
+
+        balances[from] -= tokens;
+        allowed[from][msg.sender] -= tokens;
+        balances[to] += tokens;
+
+        emit Transfer(from,to,tokens);
         return true;
     }
 }               
--- Notes.md
+++ Notes.md
@@ -3,3 +3,9 @@
 In external call your function reads x directly from calldata—no extra steps, no memory allocation
 while in public Your function body then reads x from memory.
 That copy costs a few extra gas units (for the memory allocation and the copy)
+
+# How to call functions inside of Foundry using Cast
+
+```
+cast call <contract-address> "functionName(argType)(returnType)" <arg1> --rpc-url <url>
+```
