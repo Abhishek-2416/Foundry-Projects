c69acf005be0e97f87db4b479110ab5a4dccab9f
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-06-06 08:15:14 +0530

    Made changes to the Deploy Script for DSCEngine

---
+++ 04-Foundry-Decentralized-StableCoin/script/DeployDSCEngine.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+
+contract DeployDSCEngine is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddress;
+
+    function run() external returns(DecentralizedStableCoin,DSCEngine,HelperConfig){
+        HelperConfig config = new HelperConfig();
+        (address weth,address wbtc,address wethUsdPriceFeed,address wbtcPriceFeed,uint256 deployer) = config.activeNetworkConfig();
+
+        //This is how we add addresses into the address[]
+        tokenAddresses = [weth,wbtc];
+        priceFeedAddress = [wethUsdPriceFeed,wbtcPriceFeed];
+
+        vm.startBroadcast(deployer);
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses,priceFeedAddress,address(dsc));
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+
+        return (dsc,engine,config);
+    }
+}
