29c3e7740e03e12f964e1747fa2e193b4fb39586
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-06-19 07:17:28 +0530

    Added the Interface for Rebase token and the Vault contract

---
+++ 05-Foundry-Cross-Chain-Rebase-Token/src/Vault.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IRebaseToken} from "./Interfaces/IRebaseToken.sol";
+
+/**
+ * @title Vault contract
+ * @author Abhishek Alimchandani
+ * @notice Here this is the main contract with which the user will interact with, all the functions like deposit collateral,claim rewards and all those things
+ * 
+ * 1. We need to pass the address of the rebase token in the constructor
+ * 2. Create a deposit function which accpets the collateral and mint the tokens to the user equal to the amount of collateral they have deposited
+ * 3. Create a redeem function which burns the tokens from the user and then send them back their collateral
+ * 4. Create a way to add rewards into the vault
+ */
+contract Vault{
+    IRebaseToken private immutable i_rebaseToken;
+
+    //Events
+    event Deposited(address indexed user, uint256 indexed amount);
+    event Redeem(address indexed user,uint256 indexed amount);
+
+    //Errors
+    error Vault__RedeemFailed();
+
+    constructor(IRebaseToken _rebaseTokenAddress){
+        i_rebaseToken = _rebaseTokenAddress;
+    }
+
+    // receive function to be able to send collateral to the contract
+    receive() external payable{}
+
+    /**
+     * @notice This function is used to deposit collateral into the value in order to receive the rebase tokens
+     * @dev Here we are first depositing the amount of collateral
+     * And then we are minting the tokens from the rebase tokens, directly to the user who had deposited collateral
+     */
+    function deposit() external payable{
+        // We need to use the amount of collateral the user has sent to mint tokens to the user
+        (i_rebaseToken).mint(msg.sender,msg.value);
+        emit Deposited(msg.sender,msg.value);
+    }
+
+    /**
+     * @notice Users can redeem their collateral by putting in back the Rebase tokens
+     * @dev Here we first need to burn the rebase tokens we receive from the user
+     * Then we need to transfer them back their collateral
+     */
+    function redeem(uint256 _amount) external {
+        // Burn their tokens first
+        i_rebaseToken.burn(msg.sender,_amount);
+
+        // Now send them the collateral
+        (bool success,) = payable(msg.sender).call{value: _amount}("");
+
+        if(!success){
+            revert Vault__RedeemFailed();
+        }
+
+        emit Redeem(msg.sender,_amount);
+    }
+
+    // Getter functions
+    function getTheAddressOfRebaseToken() external view returns(address){
+        return address(i_rebaseToken);
+    }
+}
