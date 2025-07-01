f76d67c0a94a7698884afb13abf8833a36c99cee
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-07-01 11:18:38 +0530

    Did the complete setUp of the Cross chain test and need more understanding

--- 05-Foundry-Cross-Chain-Rebase-Token/test/TestCrossChain.t.sol
+++ 05-Foundry-Cross-Chain-Rebase-Token/test/TestCrossChain.t.sol
@@ -7,8 +7,10 @@ import {console} from "forge-std/console.sol";
 import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {IRebaseToken} from "../src/Interfaces/IRebaseToken.sol";
+import {Client} from "ccip/contracts/src/v0.8/ccip/libraries/Client.sol";
 import {TokenPool} from "ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
 import {RateLimiter} from "ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
+import {IRouterClient} from "ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {CCIPLocalSimulatorFork,Register} from "@chainlink/local/src/ccip/CCIPLocalSimulatorFork.sol";
 import {TokenAdminRegistry} from "ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
 import {IERC20} from "ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/interfaces/IERC20.sol";
@@ -29,6 +31,8 @@ contract crossChainTest is Test{
     //Addresses
     address owner = makeAddr("owner");
     address bob = makeAddr("bob");
+
+    uint256 SEND_VALUE = 1e5;
 
     //Vault
     Vault vault;
@@ -92,7 +96,7 @@ contract crossChainTest is Test{
         TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress)
             .setPool(address(sepoliaToken), address(sepoliaPool));
 
-        configureTokenPool(sepoliaFork,sepoliaPool,arbNetworkDetails.chainSelector,)
+        configureTokenPool(sepoliaFork,address(sepoliaPool),sepoliaNetworkDetails.chainSelector,true,address(arbSepoliaPool),address(arbSepoliaToken));
 
         // Stop impersonating the owner
         vm.stopPrank();
@@ -133,12 +137,14 @@ contract crossChainTest is Test{
         TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress)
             .setPool(address(arbSepoliaToken), address(arbSepoliaPool));
 
+        configureTokenPool(arbSepoliaFork,address(arbSepoliaPool),arbNetworkDetails.chainSelector,true,address(sepoliaPool),address(sepoliaToken));
+
         // Stop impersonating the owner
         vm.stopPrank();
     }
 
     // So this is function via which we can set the chain we want to be "cross chain " with 
-    function configureTokenPool(uint256 fork,address localPool,uint64 remoteChainSelector,bool allowed,address remoteTokenAddress) public {
+    function configureTokenPool(uint256 fork,address localPool,uint64 remoteChainSelector,bool allowed,address remotePool,address remoteTokenAddress) public {
         vm.selectFork(fork);
         vm.prank(owner);
         bytes[] memory remotePoolAddresses = new bytes[](1);
@@ -148,11 +154,82 @@ contract crossChainTest is Test{
         chainsToAdd[0] = TokenPool.ChainUpdate({
             remoteChainSelector: remoteChainSelector,
             allowed: allowed,
-            remotePoolAddress: remotePoolAddresses,
-            remoteTokenAddress: remoteTokenAddress,
+            remotePoolAddress: abi.encode(remotePool),
+            remoteTokenAddress: abi.encode(remoteTokenAddress),
             outboundRateLimiterConfig: RateLimiter.Config({isEnabled: false,capacity: 0,rate:0}),
             inboundRateLimiterConfig: RateLimiter.Config({isEnabled: false,capacity: 0,rate:0})
         });
-        TokenPool(localPool).applyChainUpdates(new uint64[](0),chainsToAdd);
+        TokenPool(localPool).applyChainUpdates(chainsToAdd);
+    }
+
+    function bridgeTokens(uint256 amountToBridge,uint256 localFork, uint256 remoteFork,Register.NetworkDetails memory localNetworkDetails,Register.NetworkDetails memory remoteNetworkDetails,RebaseToken localToken,RebaseToken remoteToken) public {
+        vm.selectFork(localFork);
+        Client.EVMTokenAmount[] memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        tokenAmounts[0] = Client.EVMTokenAmount({
+            token: address(localToken),
+            amount: amountToBridge
+        });
+        //Before getting the fees we need to get the message we need to send cross chain
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(bob), // Assuming that the user is sending data to itself only the other chain
+            data: "",
+            tokenAmounts: tokenAmounts,
+            feeToken: localNetworkDetails.linkAddress,
+            extraArgs: Client._argsToBytes(Client.EVMExtraArgsV1({gasLimit: 0}))
+        });
+
+        //Now we get the fees
+        uint256 fee = IRouterClient(localNetworkDetails.routerAddress).getFee(remoteNetworkDetails.chainSelector,message);
+
+        //Pretend that we get some link
+        ccipLocalSimulatorFork.requestLinkFromFaucet(bob,fee);
+
+        //Approve Router contract for fee
+        vm.prank(bob);
+        IERC20(localNetworkDetails.linkAddress).approve(localNetworkDetails.routerAddress,fee);
+
+        //Aprove Router to allow to spend, so we can send tokens 
+        vm.prank(bob);
+        IERC20(address(localToken)).approve(localNetworkDetails.routerAddress,amountToBridge);
+
+        uint256 localBalanceBefore = localToken.balanceOf(bob);
+
+        //Now we send tokens cross chain
+        vm.prank(bob);
+        IRouterClient(localNetworkDetails.routerAddress).ccipSend(remoteNetworkDetails.chainSelector,message);
+        
+        uint256 localBalanceAfter = localToken.balanceOf(bob);
+        assertEq(localBalanceAfter,localBalanceBefore - amountToBridge);
+
+        //Now to check if the message has proporgated cross chain
+        vm.selectFork(remoteFork);
+        vm.warp(block.timestamp + 1 hours);
+        uint256 remoteBalanceBefore = remoteToken.balanceOf(bob);
+
+        //Sending the token to the remote chain
+        ccipLocalSimulatorFork.switchChainAndRouteMessage(remoteFork);
+
+        //Getting balance after
+        uint256 remoteBalanceAfter = remoteToken.balanceOf(bob);
+
+        assertEq(remoteBalanceBefore + amountToBridge, remoteBalanceAfter);
+
+        // Now we check the interest rate of this user on this chain is same to as the source chain
+        assertEq(remoteToken.getUserInterestRate(bob),localToken.getUserInterestRate(bob));
+    }
+
+    function testBridgeAllTokens() public{
+        vm.selectFork(sepoliaFork);
+        vm.deal(bob,SEND_VALUE);
+
+        // User will deposit into the vault
+        vm.prank(bob);
+        Vault(payable(address(vault))).deposit{value: SEND_VALUE}();
+        assertEq(sepoliaToken.balanceOf(bob),SEND_VALUE);
+
+        //Now we bridge tokens
+        bridgeTokens(SEND_VALUE,sepoliaFork,arbSepoliaFork,sepoliaNetworkDetails,arbNetworkDetails,sepoliaToken,arbSepoliaToken);
+
+        assertEq(arbSepoliaToken.balanceOf(bob),SEND_VALUE);
     }
 }
