02c5a45260f5d3cfb445b63a52584e188c0a3acb
Abhishek-2416 <abhishekalimchandani1624@protonmail.com>
2025-07-29 10:10:38 +0530

    Merkle Tree Script

--- 06-MerkleAirdrop-And-Signatures/src/MerkleAirdrop.sol
+++ 06-MerkleAirdrop-And-Signatures/src/MerkleAirdrop.sol
@@ -38,4 +38,12 @@ contract MerkleAirdrop  {
 
         (i_airdropToken).safeTransfer(account,amount);
     }
+
+    function getMerkleRoot() external view returns(bytes32){
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns(IERC20){
+        return i_airdropToken;
+    }
 }
